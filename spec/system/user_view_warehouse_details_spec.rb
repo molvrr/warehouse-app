0059e70a858e7b89e32a1b72fef2ed41fdc4ea0a
Mat <mateuscolvr@gmail.com>
2022-05-03 17:18:36 -0300

    Usuário vê detalhes de um galpão e volta para a página inicial

--- spec/system/user_view_warehouse_details_spec.rb
+++ spec/system/user_view_warehouse_details_spec.rb
@@ -7,7 +7,7 @@ describe 'Usuário vê detalhes de um galpão' do
                      address: 'Avenida do Aeroporto, 1000', cep: '15000-000',
                     description: 'Galpão destinado para cargas internacionais')
     # Act
-    visit('/')
+    visit(root_path)
     click_on('Aeroporto SP')
     # Assert
     expect(page).to have_content('Galpão GRU')
@@ -16,7 +16,17 @@ describe 'Usuário vê detalhes de um galpão' do
     expect(page).to have_content('Área: 100000 m²')
     expect(page).to have_content('Endereço: Avenida do Aeroporto, 1000 CEP: 15000-000')
     expect(page).to have_content('Galpão destinado para cargas internacionais')
-
-
+  end
+  it 'e volta para a tela inicial' do
+    # Arrange
+    Warehouse.create(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000,
+                     address: 'Avenida do Aeroporto, 1000', cep: '15000-000',
+                    description: 'Galpão destinado para cargas internacionais')
+    # Act
+    visit(root_path)
+    click_on('Aeroporto SP')
+    click_on('Voltar')
+    # Assert
+    expect(current_path).to eq(root_path)
   end
 end
