8a503bb054788cd91e8414e717f6aca4883bcd15
Mat <mateuscolvr@gmail.com>
2022-05-03 22:12:44 -0300

    Usuário cadastra galpão

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -2,4 +2,15 @@ class WarehousesController < ApplicationController
   def show
     @warehouse = Warehouse.find(params[:id])
   end
+
+  def new
+    @warehouse = Warehouse.new
+  end
+
+  def create
+    warehouse = Warehouse.new(params.require(:warehouse).permit(:name, :description, :code, :address, :city, :cep, :area))
+    return redirect_to root_path if warehouse.save
+
+    render :new, status: :unprocessable_entity
+  end
 end
