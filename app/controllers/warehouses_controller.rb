39c4a10a5b0583ad124f5f1e6402eb9d1359b987
Mat <mateuscolvr@gmail.com>
2022-05-06 01:13:11 -0300

    Adiciona edição de galpão

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -8,11 +8,29 @@ class WarehousesController < ApplicationController
   end
 
   def create
-    @warehouse = Warehouse.new(params.require(:warehouse).permit(:name, :description, :code,
-                                                                :address, :city, :cep, :area))
+    @warehouse = Warehouse.new(allowed_params)
     return redirect_to root_path, notice: 'Galpão cadastrado com sucesso.' if @warehouse.save
 
     flash.now[:notice] = 'Galpão não cadastrado.'
-    render :new, status: :unprocessable_entity
+    render 'new'
+  end
+
+  def edit
+    @warehouse = Warehouse.find(params[:id])
+  end
+
+  def update
+    @warehouse = Warehouse.find(params[:id])
+    return redirect_to @warehouse, notice: 'Galpão atualizado com sucesso' if @warehouse.update(allowed_params)
+
+    flash.now[:notice] = 'Não foi possível atualizar o galpão'
+    render 'edit'
+  end
+
+  private
+
+  def allowed_params
+    params.require(:warehouse).permit(:name, :description, :code,
+                                      :address, :city, :cep, :area)
   end
 end
